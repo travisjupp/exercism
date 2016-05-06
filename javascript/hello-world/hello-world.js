7ec5676c89c5e9b7338ae8fc751747ea47cf8c30
Travis Jupp <travisjupp@Traviss-MacBook-Pro.local>
2016-05-06 09:03:47 -0700

    beginning javascript with exercism. first commit of hello-world.js, spec and readme. Used with jasmine-node

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,22 @@
+//
+// This is a stub file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at hello-world.spec.js--that should give you some hints about what is
+// expected here.
+
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+
+  if (input === "Bob") {
+      return "Hello, Bob!";
+  } else if (input === "Sally") {
+      return "Hello, Sally!";
+  } else {
+      return "Hello, World!";
+  }
+
+
+};
+
+module.exports = HelloWorld;
